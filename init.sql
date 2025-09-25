092f796ab6bcc9de9bc7169f622589d0cd3e8fb9
Gabriel Andrade <gandradecortez50@gmail.com>
2025-09-25 08:25:49 -0400

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,3 +1,38 @@
+CREATE TABLE telemetria (
+    id SERIAL PRIMARY KEY,
+    unix BIGINT NOT NULL,
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION,
+    course DOUBLE PRECISION,
+    speed DOUBLE PRECISION,
+    altitude INTEGER,
+    pitch DOUBLE PRECISION,
+    roll DOUBLE PRECISION,
+    accel_magnitude DOUBLE PRECISION,
+    max_delta_accel DOUBLE PRECISION,
+    power_status SMALLINT,
+    error_status SMALLINT,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE historico_telemetria (
+    id SERIAL PRIMARY KEY,
+    telemetria_id INT REFERENCES telemetria(id) ON DELETE CASCADE,
+    unix BIGINT NOT NULL,
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION,
+    course DOUBLE PRECISION,
+    speed DOUBLE PRECISION,
+    altitude INTEGER,
+    pitch DOUBLE PRECISION,
+    roll DOUBLE PRECISION,
+    accel_magnitude DOUBLE PRECISION,
+    max_delta_accel DOUBLE PRECISION,
+    power_status SMALLINT,
+    error_status SMALLINT,
+    data_insercao TIMESTAMP DEFAULT NOW()
+);
+
 -- Função para inserir na tabela historico_telemetria
 CREATE OR REPLACE FUNCTION inserir_historico_telemetria()
 RETURNS TRIGGER AS $$
